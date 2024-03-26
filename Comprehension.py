0897347465638f8f57c58fc2a4239a03e3434b9e
ankita101995 <aankita2495@gmail.com>
2024-03-26 18:04:18 +0530

    adding three business logic

---
+++ Comprehension.py
@@ -0,0 +1,37 @@
+# addition of two numbers
+a=10
+b=30
+c=a+b
+print("addition of two number is:",c)
+
+# Substraction of two numbers
+a=100
+b=30
+c=a-b
+print("substraction of two number is:",c)
+
+lis=[1,2,3,4,5]
+for i in lis:
+    if 1%2==0:
+        print(i)
+
+# to do multiplication
+a=5
+b=9
+c=a*b
+print("this is multiplication",c)
+
+thislist =[10,20,30,"python"]
+
+for i in thislist:
+    print(i)
+
+    tup = (10, 20.3, "git", 2 + 3j, 3.14, 10)
+    print(tup)
+    print(len(tup))
+    print(type(tup))
+    print(tup[0])
+    print(tup[-1])
+    print(tup[2:4])
+    for i in tup:
+        print(i)
